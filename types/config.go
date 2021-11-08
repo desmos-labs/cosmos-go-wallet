f303e6e09f741c58307e5f38cb63d2c0b1652f35
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-11-08 13:38:15 +0100

    Added YAML tags

--- types/config.go
+++ types/config.go
@@ -1,13 +1,13 @@
 package types
 
 type ChainConfig struct {
-	RPCAddr  string `toml:"rpc_addr"`
-	GRPCAddr string `toml:"grpc_addr"`
-	GasPrice string `toml:"gas_price"`
+	RPCAddr  string `toml:"rpc_addr" yaml:"rpc_addr"`
+	GRPCAddr string `toml:"grpc_addr" yaml:"grpc_addr"`
+	GasPrice string `toml:"gas_price" yaml:"gas_price"`
 }
 
 type AccountConfig struct {
-	Bech32Prefix string `toml:"bech32_prefix"`
-	Mnemonic     string `toml:"mnemonic"`
-	HDPath       string `toml:"hd_path"`
+	Bech32Prefix string `toml:"bech32_prefix" yaml:"bech32_prefix"`
+	Mnemonic     string `toml:"mnemonic" yaml:"mnemonic"`
+	HDPath       string `toml:"hd_path" yaml:"hd_path"`
 }
