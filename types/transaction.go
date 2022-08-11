bf523e803058aec11806d46f2196281e39ede6ce
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-08-11 14:54:26 +0200

    feat: improve TransactionData to allow setting a fee granter

--- types/transaction.go
+++ types/transaction.go
@@ -4,10 +4,11 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 
 // TransactionData contains all the data about a transaction
 type TransactionData struct {
-	Messages  []sdk.Msg
-	Memo      string
-	GasLimit  uint64
-	FeeAmount sdk.Coins
+	Messages   []sdk.Msg
+	Memo       string
+	GasLimit   uint64
+	FeeAmount  sdk.Coins
+	FeeGranter sdk.AccAddress
 }
 
 // NewTransactionData builds a new TransactionData instance
@@ -32,5 +33,12 @@ func (t *TransactionData) WithGasLimit(limit uint64) *TransactionData {
 // WithFeeAmount allows to set the given fee amount
 func (t *TransactionData) WithFeeAmount(amount sdk.Coins) *TransactionData {
 	t.FeeAmount = amount
+	return t
+}
+
+// WithFeeGranter allows to set the given fee granter that will pay for fees.
+// To work properly, a fee grant must exist from the granter towards the transaction signer.
+func (t *TransactionData) WithFeeGranter(granter sdk.AccAddress) *TransactionData {
+	t.FeeGranter = granter
 	return t
 }
